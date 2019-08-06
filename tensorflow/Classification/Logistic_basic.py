455f214d52a03121a7444a8b8eef7b76cc0a6140
unknown <suho1898@naver.com>
2019-08-06 21:37:36 +0900

    [Learn] nn for XOR basic

--- tensorflow/Classification/Logistic_basic.py
+++ tensorflow/Classification/Logistic_basic.py
@@ -1,20 +1,26 @@
 import tensorflow as tf
+import numpy as np
 
-xData=[[1,2],[2,3],[3,1],[4,3],[5,3],[6,2]]
-yData=[[0],[0],[0],[1],[1],[1]]
+xData = np.array([[0,0],[0,1],[1,0],[1,1]], dtype=np.float32)
+yData = np.array([[0],[1],[1],[0]], dtype=np.float32)
 
 TrainNum=10000          #the number of training
 
 def LogisticRegression():
-    W=tf.Variable(tf.random_normal([2,1]),name='weight')
-    b=tf.Variable(tf.random_normal([1]),name='bias')
-    X=tf.placeholder(tf.float32,shape=[None,2])
-    Y=tf.placeholder(tf.float32,shape=[None,1])
+    W1 = tf.Variable(tf.random_normal([2,2]),name='weight1')
+    b1 = tf.Variable(tf.random_normal([2]),name='bias1')
+
+    W2 = tf.Variable(tf.random_normal([2,1]),name='weight2')
+    b2 = tf.Variable(tf.random_normal([1]),name='bias2')
+
+    X = tf.placeholder(tf.float32,shape=[None,2])
+    Y = tf.placeholder(tf.float32,shape=[None,1])
 
     #Hypothesis
-    H=tf.sigmoid(tf.matmul(X,W)+b)
+    H1 = tf.sigmoid(tf.matmul(X,W1)+b1)
+    H2 = tf.sigmoid(tf.matmul(H1,W2)+b2)
     #cost function
-    cost = -tf.reduce_mean(Y * tf.log(H) + (1 - Y) * tf.log(1 - H))
+    cost = -tf.reduce_mean(Y * tf.log(H2) + (1 - Y) * tf.log(1 - H2))
     #train
     learning_rate = tf.Variable(0.01)
     optimizer=tf.train.GradientDescentOptimizer(learning_rate)
